b0cf131ed4a08ea4212d1b541a0023a45584fcad
lambochan <staz@DESKTOP-D9AA9LV.localdomain>
2020-10-27 14:55:25 +0800

    php

--- resources/js/script.js
+++ resources/js/script.js
@@ -73,32 +73,7 @@ $(document).ready(function() {
 	      }
 	    }
 	});
-
-	/*---sticky navigation---*/
-	$('.js--wp-1').waypoint(function(direction) {
-		$('.js--wp-1').addClass('animated fadeIn');
-	}, {
-		offset: '50%;'
-	});
-
-	$('.js--wp-2').waypoint(function(direction) {
-		$('.js--wp-2').addClass('animated fadeInUp');
-	}, {
-		offset: '50%;'
-	});
-
-	$('.js--wp-3').waypoint(function(direction) {
-		$('.js--wp-3').addClass('animated fadeIn');
-	}, {
-		offset: '50%;'
-	});
-
-	$('.js--wp-4').waypoint(function(direction) {
-		$('.js--wp-4').addClass('animated fadeInUp');
-	}, {
-		offset: '50%;'
-	});
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 	/*----MOBILE NAV----*/
 
 	$('.js--nav-icon').click(function() {
